282232c14753bdcacbb9f927a5cf186bde7a34f8
Suvrojyoti <suvrojyotimandal1998@gmail.com>
2020-06-24 21:50:47 +0530

    added get all

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -11,15 +11,22 @@ router.post('/upload-images',  async (req, res, next) => {
         
         if(!req.body.url || !req.body.name || !req.body.type){
             res.status(422).json({
-                message: 'insufficient information'
+                message: 'Insufficient information'
             });
         }
 
+        if(req.body.type.toLowerCase()!="image" && req.body.type.toLowerCase()!="png" )
+        {
+            res.status(422).json({
+                message: 'Invalid image type, must be image/png'
+            });
+        }
         const response = await fetch(req.body.url);
         if(response.status === 404)
         {
             throw ("Invalid URL, unable to fetch");
         }
+
         const buffer = await response.buffer();
         let size = Buffer.byteLength(buffer);
         let type = await fileType.fromBuffer(buffer);
@@ -52,14 +59,19 @@ router.get('/get-images',  async (req, res, next) => {
 
     try {
 
-        if(!('nameString' in req.query)){
-            return res.status(422).json({
-                message: 'insufficient information'
-            });
-        }
-
         var limit = parseInt(req.query.limit,10)||10;
         var offset = parseInt(req.query.offset,10)||0;
+
+        if(!('nameString' in req.query)){
+            // return res.status(422).json({
+            //     message: 'insufficient information'
+            // });
+            message_response = await image.find().skip(offset).limit(limit);
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(message_response);
+            return;
+        }
         
         message_response = await image.find( { name: { $eq:req.query.nameString} } ).skip(offset).limit(limit);
         res.statusCode = 200;
